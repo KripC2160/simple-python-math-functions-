51b4333183b43eae49280e13d34ee44d3fec4c57
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2021-09-09 18:26:07 +0900

    changed the functions to return values to be more flexible

--- scripts/explicit-sequence-formula.py
+++ scripts/explicit-sequence-formula.py
@@ -3,7 +3,7 @@
 def find_diff(n1, diff, val):
     for i in range (val - 1):
         n1 += diff
-    print(n1)
+    return n1
 
-find_diff(a, b, c) #replace a with the n_1, b with difference, c with value you want to get 
+print(find_diff(a, b, c)) #replace a with the n_1, b with difference, c with value you want to get 
 
--- scripts/m_slope.py
+++ scripts/m_slope.py
@@ -2,7 +2,9 @@
 #this function finds slope of a line 
 
 def m_slope(x1, y1, x2, y2):
-    print((y2-y1),'/',(x2-x1))
-    print((y2-y1)/(x2-x1))
+    result1 = str(y2-y1)+'/'+str(x2-x1)
+    result2 = (y2-y1)/(x2-x1)
+    return result1, result2
 
-m_slope(x1, y1, x2, y2) #replace the values with the values of two points for slope
+result1, result2 = m_slope(x1, y1, x2, y2) #replace the values with the values of two points for slope
+print(result1,'\n',result2)
