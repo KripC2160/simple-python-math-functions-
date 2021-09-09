6f0b151aa5a8467884e6983437e24422240ba632
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2021-09-09 09:45:36 +0900

    m slope script

---
+++ scripts/m_slope.py
@@ -0,0 +1,8 @@
+
+#this function finds slope of a line 
+
+def m_slope(x1, y1, x2, y2):
+    print((y2-y1),'/',(x2-x1))
+    print((y2-y1)/(x2-x1))
+
+m_slope(x1, y1, x2, y2) #replace the values with the values of two points for slope
