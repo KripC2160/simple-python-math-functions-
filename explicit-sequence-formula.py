7095d62d8c261380305d60947419070791f95283
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2021-09-09 09:18:31 +0900

    explicit sequence

---
+++ explicit-sequence-formula.py
@@ -0,0 +1,9 @@
+
+###python code that finds value of term using explicit sequence###
+def find_diff(n1, diff, val):
+    for i in range (val - 1):
+        n1 += diff
+    print(n1)
+
+find_diff(a, b, c) #replace a with the n_1, b with difference, c with value you want to get 
+
