e0ca0efacb295c31c44eb3308bcab5b38ec0c4b0
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2021-09-13 21:26:58 +0900

    added arithmetic sequence folder, recursive fomula

--- scripts/Arithmetic Sequence/explicit-formula.py
+++ scripts/Arithmetic Sequence/explicit-formula.py
@@ -1,5 +1,9 @@
 
-###python code that finds value of term using explicit sequence###
+###python code that finds value of term using explicit formula###
+
+#original mathematical formula: a_n = a_1 + d(n-1)
+# a is the value, n is the term, d is the difference 
+
 def find_diff(n1, diff, val):
     for i in range (val - 1):
         n1 += diff
---
+++ scripts/Arithmetic Sequence/recusive-formula.py
@@ -0,0 +1,13 @@
+
+###python code that finds value of term using recursive sequence###
+"""
+Warning: recursive formula is only good if you want to find the unknown value after known (e.g. n_35 -> n_36). 
+If you want to find values that are not close to the known variables (e.g. n_35 -> n_205), then use the explicit-formula.py instead
+"""
+#original mathematical formula: a_n = a_(n-1) + d
+# a is the value, n is the term, d is the difference 
+
+def find_recursive(answer, difference):
+    return answer + difference #yes, it's this simple 
+
+print(find_recursive(7, 7)) #replace a with the answer with term before the one you want to find, replace c for difference
