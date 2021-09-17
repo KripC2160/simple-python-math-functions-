bc7bc01215798b0bee2a074e3fc61a2c4ea98488
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2021-09-17 22:50:40 +0900

    completed fibonacci

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -1,17 +1,17 @@
 
 
 #fibonnaci sequence
+#prints out e.g. 1,1,2,3,5,7,12
 #formula fn = fn-1 + fn-2
 
 def fibonacci(forloop):
-    bef, aft = 1
+    num = [0, 1] #array that holds the current and previous values
     for i in range(forloop):
-        if i < 2:
-            return 1
-            print("1")
+        if i < 1:
+            print(num[1])
         else:
-            res = bef + aft
-            return res
-            print(res)
+            print(num[0]+num[1])
+            num.append(num[0]+num[1])
+            num.pop(0)
 
-fibonacci(forloop)
+fibonacci(forloop) #replace for loops with amount value you want to get
