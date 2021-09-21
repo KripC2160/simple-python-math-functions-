9b4aa45909d96a87e4cc2ee36ddcded8d92587ab
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2021-09-21 12:45:35 +0900

    updated

--- scripts/Arithmetic Sequence/member-of-n.py
+++ scripts/Arithmetic Sequence/member-of-n.py
@@ -10,8 +10,8 @@
 
 def is_member(num, n1):
     if (num - n1) % 2 == 0:
-        return (num -n1) % 2
+        return (num -n1) / 2
     else: 
-        return False
+        return False #Returns false is not part of the member
 
-print(is_member(523, 431))
+print(is_member(30, 2))
